ecabe428bce4d2eacf34c619aa42e11092e0ed0b
NiyazBiyaz <niyazik114422@gmail.com>
2026-03-09 12:16:35 +0500

    Initial commit

---
+++ PySharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PySharp;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        foreach (string arg in args)
+            Console.WriteLine($"Hello, {arg}!");
+    }
+}
